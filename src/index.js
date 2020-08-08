51cf151d0ec6bc1093a3a3d6177003702640a6b3
Adamlatorraca <adam.latorraca@gmail.com>
2020-08-07 21:18:17 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById('create-task-form');
+  let task = document.getElementById('new-task-description');
+  let ul = document.getElementById('tasks');
+
+  form.addEventListener('submit', function(e) {
+    e.preventDefault()
+    if (task.value) {
+      ul.innerHTML += `<li>${task.value}</li>`
+      task.value = ''
+    }
+  });
 });
